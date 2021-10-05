42d0e870e391bf05d6a2e099a264c01d315d3f30
yabsiraa <88306942+yabsiraa@users.noreply.github.com>
2021-10-05 21:00:03 +0300

    Create strcpy.c

---
+++ strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcpy -  a function that copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer pointed to by dest
+ * @src: the buffer to be copied from
+ * @dest: destination of the copied string
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != 0)
+	{
+		len++;
+	}
+	len++;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
